6f78479520472f7839d777467b1e097ca60d3fbc
Will Shahda <will.shahda@gmail.com>
2019-02-11 16:35:51 -0500

    Create HOC for providing web3 in props

--- client/src/utils/getWeb3.js
+++ client/src/utils/getWeb3.js
@@ -1,38 +1,50 @@
 import Web3 from "web3";
 
-const getWeb3 = () =>
+const getWeb3 = async (requireAccounts, resolve, reject) => {
+  // Modern dapp browsers...
+  if (requireAccounts) {
+    if (window.ethereum) {
+      const web3 = new Web3(window.ethereum);
+      try {
+        // Request account access if needed
+        await window.ethereum.enable();
+        // Accounts now exposed
+        resolve(web3);
+      } catch (error) {
+        reject(error);
+      }
+    // Legacy dapp browsers...
+    } else if (window.web3) {
+      // Use Mist/MetaMask's provider.
+      const web3 = new Web3(window.web3.currentProvider);
+      console.log("Injected web3 detected.");
+      resolve(web3);
+    }
+  }
+
+  // Default provider when accounts are not required
+  let defaultNetwork;
+  if (process.env.NODE_ENV === 'production') {
+    defaultNetwork = process.env.REACT_APP_PROD_NETWORK;
+  } else {
+    defaultNetwork = process.env.REACT_APP_DEV_NETWORK;
+  }
+  const provider = new Web3.providers.HttpProvider(defaultNetwork);
+  const web3 = new Web3(provider);
+  console.log("No web3 instance injected, using default provider.");
+  resolve(web3);
+}
+
+export default requireAccounts => {
   new Promise((resolve, reject) => {
     // Wait for loading completion to avoid race conditions with web3 injection timing.
-    window.addEventListener("load", async () => {
-      // Modern dapp browsers...
-      if (window.ethereum) {
-        const web3 = new Web3(window.ethereum);
-        try {
-          // Request account access if needed
-          await window.ethereum.enable();
-          // Acccounts now exposed
-          resolve(web3);
-        } catch (error) {
-          reject(error);
-        }
-      }
-      // Legacy dapp browsers...
-      else if (window.web3) {
-        // Use Mist/MetaMask's provider.
-        const web3 = window.web3;
-        console.log("Injected web3 detected.");
-        resolve(web3);
-      }
-      // Fallback to localhost; use dev console port by default...
-      else {
-        const provider = new Web3.providers.HttpProvider(
-          "http://127.0.0.1:9545"
-        );
-        const web3 = new Web3(provider);
-        console.log("No web3 instance injected, using Local web3.");
-        resolve(web3);
-      }
+    window.addEventListener("load", () => {
+      getWeb3(requireAccounts, resolve, reject);
     });
-  });
 
-export default getWeb3;
+    // If document is already loaded, no race conditions are possible.
+    if (document.readyState === 'complete') {
+      getWeb3(requireAccounts, resolve, reject);
+    }
+  });
+};
---
+++ client/src/utils/withWeb3.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import getWeb3 from './getWeb3';
+import MetaMask from '../MetaMask';
+import { Dimmer, Loader } from 'semantic-ui-react';
+
+/**
+ * WrappedComponent should use componentWillReceiveProps to implement web3
+ * calls instead of componentDidMount.
+ */
+function withWeb3(WrappedComponent, requireAccounts=false) {
+  return class extends Component {
+    state = {
+      web3: null,
+      accounts: null,
+      loading: false
+    };
+
+    async componentDidMount() {
+      this.setState({ loading: true });
+
+      try {
+        // Get network provider and web3 instance.
+        const web3 = await getWeb3(requireAccounts);
+
+        // Use web3 to get the user's accounts.
+        const accounts = await web3.eth.getAccounts();
+
+        this.setState({ web3, accounts });
+      } catch (error) {
+        // Catch any errors for any of the above operations.
+        console.error(error);
+      }
+
+      this.setState({ loading: false });
+    };
+
+    render() {
+      const { web3, accounts, loading } = this.state;
+
+      const hasMetaMask = accounts && accounts.length > 0;
+      if (requireAccounts && !hasMetaMask && !loading) {
+        return <MetaMask />;
+      }
+
+      return (
+        <>
+          <Dimmer active={loading} page>
+            <Loader />
+          </Dimmer>
+          <WrappedComponent
+            {...this.props}
+            web3={web3}
+            accounts={accounts}
+          />
+        </>
+      );
+    }
+  }
+}
+
+export default withWeb3;
