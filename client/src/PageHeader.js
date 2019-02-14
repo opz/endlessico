718b3dd43f39cfc8d2f3c35cbd42f622328bbd94
Will Shahda <will.shahda@gmail.com>
2019-02-14 16:38:22 -0500

    Implement full screen page header

---
+++ client/src/PageHeader.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button, Divider, Grid, Header } from 'semantic-ui-react';
+
+import './PageHeader.css';
+
+export default props => {
+  const { title, subtitle, callToAction, onCallToAction } = props;
+
+  return (
+    <div className="PageHeader">
+      <Grid verticalAlign="middle" textAlign="center">
+        <Grid.Row>
+          <Grid.Column>
+            <Header as="h1" textAlign="center" className="title">
+              {title}
+              <Header.Subheader>
+                {subtitle}
+              </Header.Subheader>
+            </Header>
+            <Divider hidden />
+            <Button primary onClick={onCallToAction}>{callToAction}</Button>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
+  );
+}
