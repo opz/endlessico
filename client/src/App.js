2fe32970bb6cc23605a022a75ac3a931992374a2
Will Shahda <will.shahda@gmail.com>
2019-02-14 16:39:02 -0500

    Update theme styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Routes from './Routes';
 
-import 'semantic-ui-forest-themes/semantic.flatly.min.css';
+import 'semantic-ui-forest-themes/semantic.yeti.min.css';
 import './App.css';
 
 class App extends Component {
